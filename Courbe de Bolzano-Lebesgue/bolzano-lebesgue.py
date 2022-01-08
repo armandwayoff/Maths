f6816604967c46861c05d0178cd43d5614739c1f
Armand Wayoff <armandwayoff@lavabit.com>
2022-01-08 15:26:48 +0100

    Update bolzano-lebesgue.py

--- Courbe de Bolzano-Lebesgue/bolzano-lebesgue.py
+++ Courbe de Bolzano-Lebesgue/bolzano-lebesgue.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
-n = 4
+n = 8
 x, y = [k / 3 ** n for k in range(3 ** n + 1)], [0, 1]
 for _ in range(n):
     y = (y1 := [c * 2/3 for c in y]) + [2/3 - 1/2 * c for c in y1[1:-1]] + [c + 1/3 for c in y1]
-plt.plot(x, y, linewidth=0.8)
+plt.title("Courbe de Bolzano-Lebesgue d'ordre " + str(n))
+plt.plot(x, y, linewidth=0.08)
 plt.show()
