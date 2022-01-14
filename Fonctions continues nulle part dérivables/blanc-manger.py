1a110e9d869dcbe7e3dcdb7e499cbe165c92221a
Armand Wayoff <armandwayoff@lavabit.com>
2022-01-14 16:01:44 +0100

    Update blanc-manger.py

--- Fonctions continues nulle part dérivables/blanc-manger.py
+++ Fonctions continues nulle part dérivables/blanc-manger.py
@@ -1,19 +1,10 @@
 import matplotlib.pyplot as plt
 n = 15
-x, y = [0, 1/2, 1], [0, 1/2, 0]
-# plt.plot(x, y)
+x, y = [i / 2 ** n for i in range(2 ** n + 1)], [0, 1/2, 0]
 for k in range(1, n):
-    x = [i / 2 ** (k+1) for i in range(2 ** (k+1) + 1)]
-    gene = 2 ** k * [0, 1 / 2 ** (k+1)] + [0]
-    echan = []
-    for i in range(len(y) - 1):
-        echan.append(y[i])
-        echan.append((y[i] + y[i + 1]) / 2)
-    echan.append(0)
-    Y = []
-    for i in range(len(echan)):
-        Y.append(echan[i] + gene[i])
-    y = Y
+    triangles = 2 ** k * [0, 1/2 ** (k+1)] + [0]
+    echan = [f for i in range(len(y) - 1) for f in (y[i], (y[i] + y[i+1]) / 2)] + [0]
+    y = [echan[i] + triangles[i] for i in range(len(echan))]
 plt.title("Courbe du blanc-manger d'ordre " + str(n))
 plt.plot(x, y, linewidth=0.8)
 plt.show()
